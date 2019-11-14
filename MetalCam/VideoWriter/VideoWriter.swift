e5d124f339d40733a99a4514b6311c9b5ebc1eae
Andrew Galushka <andrewgalushka@yahoo.com>
2019-11-14 18:52:58 +0200

    Refactor VideoWriter
    rf

--- MetalCam/VideoWriter/VideoWriter.swift
+++ MetalCam/VideoWriter/VideoWriter.swift
@@ -13,6 +13,8 @@ class VideoWriter: VideoWritable {
     private var assetWriterInput: AVAssetWriterInput?
     private var assetWriterInputAdapter: AVAssetWriterInputPixelBufferAdaptor?
     
+    private let processingQueue = DispatchQueue(label: "videoWriter.appending.queue")
+    
     private var state: State = .inactive {
         didSet {
             switch state {
@@ -25,51 +27,76 @@ class VideoWriter: VideoWritable {
     private(set) var isRecording: Bool = false
     
     func startRecording(url: URL) throws {
+        self.processingQueue.async { [weak self] in
+            do {
+                guard let `self` = self else { return }
+                assert(.inactive == self.state, "Unexpected state")
+                
+                let assetWriter = try AVAssetWriter(url: url, fileType: .mov)
+                let assetWriterInput = AVAssetWriterInput(mediaType: .video, outputSettings: nil)
+                assetWriterInput.expectsMediaDataInRealTime = true
+                let assetWriterInputAdapter = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterInput, sourcePixelBufferAttributes: nil)
+                
+                self.assetWriter = assetWriter
+                self.assetWriterInput = assetWriterInput
+                self.assetWriterInputAdapter = assetWriterInputAdapter
+                assetWriter.add(assetWriterInput)
+                
+                self.state = .recording(assetWriterInputAdapter, assetWriter)
+            } catch {
+                
+            }
+            
+        }
         
-        assert(.inactive == self.state, "Unexpected state")
-        
-        let assetWriter = try AVAssetWriter(url: url, fileType: .mov)
-        let assetWriterInput = AVAssetWriterInput(mediaType: .video, outputSettings: nil)
-        assetWriterInput.expectsMediaDataInRealTime = true
-        let assetWriterInputAdapter = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterInput, sourcePixelBufferAttributes: nil)
-        self.assetWriter = assetWriter
-        self.assetWriterInput = assetWriterInput
-        self.assetWriterInputAdapter = assetWriterInputAdapter
-        
-        self.state = .recording(assetWriterInputAdapter, assetWriter)
-        
-        assetWriter.startWriting()
     }
     
     func update(sampleBuffer: CMSampleBuffer) {
-        switch state {
-        case .inactive:
-            assert(false, "Unexpected state, should be .recording")
-            return
-        case .recording(let pixelBufferAdapter, _):
-            self.appendSampleBuffer(sampleBuffer, adapter: pixelBufferAdapter)
-        }
-    }
-    
-    func finish(completion: @escaping (URL) -> Void) {
-        switch state {
-        case .inactive:
-            assert(false, "Unexpected state, should be .recording")
-            return
-        case .recording(_, let assetWriter):
-            assetWriter.finishWriting { [weak self] in
-                completion(assetWriter.outputURL)
-                self?.reset()
+        self.processingQueue.async { [weak self] in
+            guard let `self` = self else { return }
+            switch self.state {
+            case .inactive:
+                assert(false, "Unexpected state, should be .recording")
+                return
+            case .recording(let pixelBufferAdapter, let assetWriter):
+                self.appendSampleBuffer(sampleBuffer, adapter: pixelBufferAdapter, assetWriter: assetWriter)
             }
         }
     }
     
-    private func appendSampleBuffer(_ sampleBuffer: CMSampleBuffer, adapter: AVAssetWriterInputPixelBufferAdaptor) {
+    func finish(completion: @escaping (URL) -> Void) {
+        self.processingQueue.async {  [weak self] in
+            guard let `self` = self else { return }
+            switch self.state {
+            case .inactive:
+                assert(false, "Unexpected state, should be .recording")
+                return
+            case .recording(let adapter, let assetWriter):
+                adapter.assetWriterInput.markAsFinished()
+                assetWriter.finishWriting { [weak self] in
+                    completion(assetWriter.outputURL)
+                    self?.reset()
+                }
+            }
+        }
+    }
+    
+    private func appendSampleBuffer(_ sampleBuffer: CMSampleBuffer, adapter: AVAssetWriterInputPixelBufferAdaptor, assetWriter: AVAssetWriter) {
         let presentationTime = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
         guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        
-        if adapter.assetWriterInput.isReadyForMoreMediaData {
-            adapter.append(imageBuffer, withPresentationTime: presentationTime)
+   
+        if assetWriter.status == .unknown {
+            assetWriter.startWriting()
+            assetWriter.startSession(atSourceTime: presentationTime)
+        }
+    
+        if adapter.assetWriterInput.isReadyForMoreMediaData, let pool = adapter.pixelBufferPool {
+            var pixelBufferCopy: CVPixelBuffer?
+            CVPixelBufferPoolCreatePixelBuffer(nil, pool, &pixelBufferCopy)
+            
+            if let pixelBufferCopy = pixelBufferCopy {
+                assert(adapter.append(pixelBufferCopy, withPresentationTime: presentationTime), "Something went wrong")
+            }
         }
     }
     
